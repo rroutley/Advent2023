f139cd34e43bf012864ac1321fbfe5a4589252a7
Richard Routley <Richard.Routley@barnett-waddingham.co.uk>
2023-12-08 10:29:58 +0000

    Day 8 Part 2 - brute force

--- Day8/Puzzle8.cs
+++ Day8/Puzzle8.cs
@@ -1,5 +1,4 @@
 
-using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 class Puzzle8 : IPuzzle
@@ -10,7 +9,7 @@ class Puzzle8 : IPuzzle
     {
 
         var lines = File.ReadAllLines("Day8/input.txt");
-        //   lines = sample.Replace("\n", "").Split(['\r']);
+        lines = sample.Replace("\n", "").Split(['\r']);
 
         var instructions = lines[0].ToArray();
 
@@ -29,7 +28,12 @@ class Puzzle8 : IPuzzle
             directions.Add(start, (left, right));
 
         }
+        //Part1(instructions, directions);
+        Part2(instructions, directions);
+    }
 
+    private static void Part1(char[] instructions, Dictionary<string, (string Left, string Right)> directions)
+    {
         int p = 0;
         var pos = "AAA";
         while (pos != "ZZZ")
@@ -47,16 +51,59 @@ class Puzzle8 : IPuzzle
 
         }
         System.Console.WriteLine($"Reached {pos} in {p} moves");
+    }
 
+    private static void Part2(char[] instructions, Dictionary<string, (string Left, string Right)> directions)
+    {
+        long p = 0;
+
+        var positions = directions.Keys.Where(k => k.EndsWith('A')).ToList();
+
+        bool done = false;
+        while (!done)
+        {
+
+            var direction = instructions[p % instructions.Length];
+            p++;
+
+            done = true;
+            for (int i = 0; i < positions.Count; i++)
+            {
+                var pos = positions[i];
+                //      System.Console.WriteLine($"From {pos} go {direction}");
+                pos = direction switch
+                {
+                    'L' => directions[pos].Left,
+                    'R' => directions[pos].Right,
+                    _ => throw new NotImplementedException()
+                };
+
+                if (!pos.EndsWith('Z'))
+                    done = false;
+
+                positions[i] = pos;
+            }
+
+
+            if (p % 100_000_000 == 0)
+            {
+                System.Console.WriteLine(p);
+            }
+        }
+        System.Console.WriteLine($"Reached {string.Join(", ", positions)} in {p} moves");
     }
 
 
-
     string sample = """
-                    LLR
+                    LR
 
-                    AAA = (BBB, BBB)
-                    BBB = (AAA, ZZZ)
-                    ZZZ = (ZZZ, ZZZ)
+                    11A = (11B, XXX)
+                    11B = (XXX, 11Z)
+                    11Z = (11B, XXX)
+                    22A = (22B, XXX)
+                    22B = (22C, 22C)
+                    22C = (22Z, 22Z)
+                    22Z = (22B, 22B)
+                    XXX = (XXX, XXX)
                     """;
 }
