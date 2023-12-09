399be862780a00a605d59904c65e36b3e24afc8f
Richard Routley <Richard.Routley@barnett-waddingham.co.uk>
2023-12-09 05:17:09 +0000

    Day 9 Part 2

--- Day9/Puzzle9.cs
+++ Day9/Puzzle9.cs
@@ -6,23 +6,37 @@ class Puzzle9 : IPuzzle
     {
 
         var lines = File.ReadAllLines("Day9/input.txt");
-        //  lines = sample.Replace("\n", "").Split(['\r']);
+        //     lines = sample.Replace("\n", "").Split(['\r']);
 
 
-var total=0;
-
+        var total = 0;
         foreach (var line in lines)
         {
             var history = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
             var next = history[^1] + PredictNext(history);
 
-total+=next;
+            total += next;
             System.Console.WriteLine(next);
 
         }
 
         System.Console.WriteLine("Answer = {0}", total);
+
+
+        total = 0;
+        foreach (var line in lines)
+        {
+            var history = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var next = history[0] - PredictPrevious(history);
+
+            total += next;
+            System.Console.WriteLine(next);
+
+        }
+        System.Console.WriteLine("Answer = {0}", total);
+
     }
 
     private int PredictNext(int[] history)
@@ -32,15 +46,36 @@ total+=next;
             return 0;
         }
 
+        int[] differences = CalcDifferences(history);
+
+        var value = PredictNext(differences);
+
+        return differences[^1] + value;
+    }
+
+    private static int[] CalcDifferences(int[] history)
+    {
         var differences = new int[history.Length - 1];
         for (int i = 0; i < history.Length - 1; i++)
         {
             differences[i] = history[i + 1] - history[i];
         }
 
-        var value = PredictNext(differences);
+        return differences;
+    }
 
-        return differences[^1] + value;
+    private int PredictPrevious(int[] history)
+    {
+        if (history.All(h => h == 0))
+        {
+            return 0;
+        }
+
+        int[] differences = CalcDifferences(history);
+
+        var value = PredictPrevious(differences);
+
+        return differences[0] - value;
     }
 
     string sample = """
