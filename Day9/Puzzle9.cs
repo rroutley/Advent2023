7c4b45965b59c739bb7dca50e180a6c3ac2a4abb
Richard Routley <Richard.Routley@barnett-waddingham.co.uk>
2023-12-09 05:11:33 +0000

    Day 9 Part 1

--- Day9/Puzzle9.cs
+++ Day9/Puzzle9.cs
@@ -6,10 +6,46 @@ class Puzzle9 : IPuzzle
     {
 
         var lines = File.ReadAllLines("Day9/input.txt");
-        lines = sample.Replace("\n", "").Split(['\r']);
+        //  lines = sample.Replace("\n", "").Split(['\r']);
+
+
+var total=0;
+
+        foreach (var line in lines)
+        {
+            var history = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var next = history[^1] + PredictNext(history);
+
+total+=next;
+            System.Console.WriteLine(next);
+
+        }
+
+        System.Console.WriteLine("Answer = {0}", total);
+    }
+
+    private int PredictNext(int[] history)
+    {
+        if (history.All(h => h == 0))
+        {
+            return 0;
+        }
+
+        var differences = new int[history.Length - 1];
+        for (int i = 0; i < history.Length - 1; i++)
+        {
+            differences[i] = history[i + 1] - history[i];
+        }
+
+        var value = PredictNext(differences);
+
+        return differences[^1] + value;
     }
 
     string sample = """
-
+                    0 3 6 9 12 15
+                    1 3 6 10 15 21
+                    10 13 16 21 30 45
                     """;
 }
