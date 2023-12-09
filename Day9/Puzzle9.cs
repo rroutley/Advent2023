e0ac5d820e06ac5502fd223b7f425e1734e0cc06
Richard Routley <Richard.Routley@barnett-waddingham.co.uk>
2023-12-09 04:59:52 +0000

    setup day 9

---
+++ Day9/Puzzle9.cs
@@ -0,0 +1,15 @@
+using System.Text.RegularExpressions;
+
+class Puzzle9 : IPuzzle
+{
+    public void Excute()
+    {
+
+        var lines = File.ReadAllLines("Day9/input.txt");
+        lines = sample.Replace("\n", "").Split(['\r']);
+    }
+
+    string sample = """
+
+                    """;
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿var puzzle = new Puzzle8();
+﻿using System.Reflection;
+
+var lastPuzzleType = Assembly.GetEntryAssembly()
+                             .GetTypes()
+                             .Where(t => typeof(IPuzzle).IsAssignableFrom(t) && t.IsClass)
+                             .OrderByDescending(t => t.Name)
+                             .First();
+
+var puzzle = Activator.CreateInstance(lastPuzzleType) as IPuzzle;
+
+
+System.Console.WriteLine("Running {0}", lastPuzzleType.Name);
 puzzle.Excute();
