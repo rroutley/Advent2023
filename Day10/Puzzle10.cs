ed3ec3f25f8807d1554545f266a2ac37bffde639
Richard Routley <Richard.Routley@barnett-waddingham.co.uk>
2023-12-10 09:17:14 +0000

    Day 10 Part 2

--- Day10/Puzzle10.cs
+++ Day10/Puzzle10.cs
@@ -1,6 +1,7 @@
 using System.Data;
 using System.Drawing;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
 using Point2D = (int x, int y);
@@ -44,6 +45,46 @@ class Puzzle10 : IPuzzle
         [Ground] = [],
     };
 
+    private IReadOnlyDictionary<char, int[]> bitmaps = new Dictionary<char, int[]>
+    {
+        [Vertical] = [
+                0b010,
+                0b010,
+                0b010
+                ],
+        [Horizontal] = [
+                0b000,
+                0b111,
+                0b000
+                ],
+        [TopLeft] = [
+                0b000,
+                0b011,
+                0b010
+                ],
+        [BottomRight] = [
+                0b010,
+                0b110,
+                0b000
+                ],
+        [TopRight] = [
+                0b000,
+                0b110,
+                0b010
+                ],
+        [BottomLeft] = [
+                0b010,
+                0b011,
+                0b000
+                ],
+        [Start] = [
+                0b111,
+                0b111,
+                0b111
+                ],
+    };
+
+
     private int rows, cols;
     private char[,] grid;
 
@@ -57,9 +98,145 @@ class Puzzle10 : IPuzzle
 
         Part1(startingPosition);
 
+
+        var points = FollowPath(startingPosition).ToList();
+
+        DumpAsAscii(points);
+
+        var canvas = MakeCanvas(points);
+        FloodFill(canvas, (0, 0));
+
+        SaveAsPgm(canvas, "Day10/output.pgm");
+
+        var interoirCount = CountInterior(canvas);
+        System.Console.WriteLine("Part 2 = {0}", interoirCount);
     }
 
-    private (int x, int y) SetupGrid(string[] lines)
+    const byte Black = 0;
+    const byte DarkGrey = 1;
+    const byte LightGrey = 2;
+    const byte White = 3;
+
+    const byte NotSet = Black;
+    const byte PipeColor = White;
+    const byte PipeSurround = DarkGrey;
+    const byte Outside = LightGrey;
+
+    private int CountInterior(byte[,] canvas)
+    {
+        int interior = 0;
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                // Is the center pixel zero?
+                if (canvas[3 * c + 1, 3 * r + 1] == Black)
+                {
+                    interior++;
+                }
+            }
+        }
+        return interior;
+    }
+
+    private void SaveAsPgm(byte[,] canvas, string fileName)
+    {
+        using var writer = File.CreateText(fileName);
+        writer.NewLine = "\n";
+
+        var height = canvas.GetLength(0);
+        var width = canvas.GetLength(1);
+
+        writer.WriteLine("P2");
+        writer.WriteLine("# Day 10 AoC");
+        writer.WriteLine($"{canvas.GetLength(0)} {canvas.GetLength(1)}");
+        writer.WriteLine(3); // Maximum greyscale value
+        for (int r = 0; r < canvas.GetLength(1); r++)
+        {
+            for (int c = 0; c < canvas.GetLength(0); c++)
+            {
+                writer.Write(canvas[c, r]);
+                writer.Write(" ");
+            }
+            writer.WriteLine();
+        }
+
+    }
+
+
+    private byte[,] MakeCanvas(List<Point2D> points)
+    {
+        var canvas = new byte[cols * 3, rows * 3];
+        foreach (var (x, y) in points)
+        {
+            var bitmap = bitmaps[grid[x, y]];
+
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    canvas[3 * x + i, 3 * y + j] = ((bitmap[j] & (4 >> i)) != 0) ? PipeColor : PipeSurround;
+                }
+            }
+        }
+        return canvas;
+    }
+
+
+
+    private void FloodFill(byte[,] canvas, (int, int) value)
+    {
+        var width = canvas.GetLength(0);
+        var height = canvas.GetLength(1);
+
+        var queue = new Queue<Point2D>();
+        queue.Enqueue(value);
+        while (queue.Count > 0)
+        {
+            var pos = queue.Dequeue();
+
+            for (int i = 0; i < 4; i++)
+            {
+                var dir = (Direction)i;
+                Point2D newPos = (pos.x + deltas[dir].x, pos.y + deltas[dir].y);
+                // Check still within grid
+                if (newPos.x < 0 || newPos.x >= width || newPos.y < 0 || newPos.y >= height)
+                {
+                    continue;
+                }
+
+                byte pixel = canvas[newPos.x, newPos.y];
+                if (pixel == NotSet || pixel == PipeSurround)
+                {
+                    canvas[newPos.x, newPos.y] = Outside;
+                    queue.Enqueue(newPos);
+                }
+
+            }
+
+        }
+    }
+
+    private void DumpAsAscii(List<(int x, int y)> points)
+    {
+        var output = new char[rows][];
+        for (int i = 0; i < rows; i++)
+        {
+            output[i] = new String(' ', cols).ToArray();
+        }
+
+        foreach (var point in points)
+        {
+            output[point.y][point.x] = grid[point.x, point.y];
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            System.Console.WriteLine(new string(output[i]));
+        }
+    }
+
+    private Point2D SetupGrid(string[] lines)
     {
         rows = lines.Length;
         cols = lines[0].Length;
@@ -86,7 +263,14 @@ class Puzzle10 : IPuzzle
         return startingPosition;
     }
 
-    private void Part1((int x, int y) startingPosition)
+    private void Part1(Point2D startingPosition)
+    {
+        int total = FollowPath(startingPosition).Count();
+
+        System.Console.WriteLine("Answer = {0}", total / 2);
+    }
+
+    private IEnumerable<Point2D> FollowPath(Point2D startingPosition)
     {
         HashSet<Point2D> history = new HashSet<Point2D>();
 
@@ -102,11 +286,10 @@ class Puzzle10 : IPuzzle
             position = possible.Where(p => !history.Contains(p)).First();
             history.Add(position);
 
-            total++;
+            yield return position;
 
         } while (position != startingPosition);
 
-        System.Console.WriteLine("Answer = {0}", total / 2);
     }
 
     private IEnumerable<Point2D> CandidateMoves(Point2D position, bool skipChecks)
