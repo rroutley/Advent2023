b5e485f9ce6f0e9ecfb2c1346a45956f559fec38
Richard Routley <Richard.Routley@barnett-waddingham.co.uk>
2023-12-15 04:01:46 +0000

    Day 14 Part 2

--- Day14/Puzzle14.cs
+++ Day14/Puzzle14.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using System.Runtime.CompilerServices;
 using Point2D = (int x, int y);
 
 class Puzzle14 : IPuzzle
@@ -24,6 +25,51 @@ class Puzzle14 : IPuzzle
 
         System.Console.WriteLine($"Answer ={total}");
 
+        rocks = ParseLines(lines);
+        Part2(rocks);
+
+    }
+
+    private void Part2(char[,] rocks)
+    {
+        // Run until reach steady state;
+        var scores = new List<(long, long)>();
+        var cycleScores = new List<(long, long)>();
+        long hashcode = 0;
+
+        int leadinLength = SpinUntilRepeat(rocks, scores, ref hashcode);
+
+        int cycleLength = SpinUntilRepeat(rocks, cycleScores, ref hashcode);
+
+        System.Console.WriteLine(leadinLength);
+        System.Console.WriteLine(cycleLength);
+
+        var answer = (1_000_000_000 - leadinLength - 1) % cycleLength;
+
+        System.Console.WriteLine($"Answer = {cycleScores[answer].Item1}");
+
+
+
+        int SpinUntilRepeat(char[,] rocks, List<(long, long)> scores, ref long hashcode)
+        {
+            var h = new HashSet<long>
+            {
+                hashcode
+            };
+
+            bool match;
+            int cycleLength = 0;
+            do
+            {
+                Spin(rocks, 1);
+                hashcode = HashCode(rocks);
+                scores.Add((Score(rocks), hashcode));
+
+                match = h.Add(hashcode);
+                cycleLength++;
+            } while (match);
+            return cycleLength;
+        }
     }
 
     private char[,] ParseLines(string[] lines)
@@ -52,6 +98,17 @@ class Puzzle14 : IPuzzle
         return rocks;
     }
 
+    private void Spin(char[,] rocks, long times)
+    {
+        for (long i = 0; i < times; i++)
+        {
+            TiltNorth(rocks);
+            TiltWest(rocks);
+            TiltSouth(rocks);
+            TiltEast(rocks);
+        }
+    }
+
     private void TiltNorth(char[,] rocks)
     {
         bool changed;
@@ -74,6 +131,69 @@ class Puzzle14 : IPuzzle
         } while (changed);
     }
 
+    private void TiltWest(char[,] rocks)
+    {
+        bool changed;
+        do
+        {
+            changed = false;
+            for (var r = 0; r < rows; r++)
+            {
+                for (int c = cols - 2; c >= 0; c--)
+                {
+                    if (rocks[c, r] == '.' && rocks[c + 1, r] == 'O')
+                    {
+                        rocks[c, r] = 'O';
+                        rocks[c + 1, r] = '.';
+                        changed = true;
+                    }
+
+                }
+            }
+        } while (changed);
+    }
+    private void TiltSouth(char[,] rocks)
+    {
+        bool changed;
+        do
+        {
+            changed = false;
+            for (var r = 1; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    if (rocks[c, r] == '.' && rocks[c, r - 1] == 'O')
+                    {
+                        rocks[c, r] = 'O';
+                        rocks[c, r - 1] = '.';
+                        changed = true;
+                    }
+
+                }
+            }
+        } while (changed);
+    }
+    private void TiltEast(char[,] rocks)
+    {
+        bool changed;
+        do
+        {
+            changed = false;
+            for (var r = 0; r < rows; r++)
+            {
+                for (int c = 1; c < cols; c++)
+                {
+                    if (rocks[c, r] == '.' && rocks[c - 1, r] == 'O')
+                    {
+                        rocks[c, r] = 'O';
+                        rocks[c - 1, r] = '.';
+                        changed = true;
+                    }
+
+                }
+            }
+        } while (changed);
+    }
     long Score(char[,] rocks)
     {
         long score = 0;
@@ -90,7 +210,21 @@ class Puzzle14 : IPuzzle
         return score;
     }
 
-
+    long HashCode(char[,] rocks)
+    {
+        long score = 0;
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                if (rocks[c, r] == 'O')
+                {
+                    score += (rows - r) + (rows + 1) * c;
+                }
+            }
+        }
+        return score;
+    }
 
     void Dump(char[,] rocks)
     {
