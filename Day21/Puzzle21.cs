6d77605abcc4f529d9e97e5add613cacaf7728a9
Richard Routley <Richard.Routley@barnett-waddingham.co.uk>
2023-12-24 08:08:02 +0000

    Day 21 Part 1

---
+++ Day21/Puzzle21.cs
@@ -0,0 +1,146 @@
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Runtime.CompilerServices;
+using Point2d = (int x, int y);
+
+class Puzzle21 : IPuzzle
+{
+
+    const byte Rock = 0;
+    const byte Garden = 1;
+
+    const byte Step = 2;
+
+    private int rows;
+    private int cols;
+    public void Excute()
+    {
+
+        var lines = File.ReadAllLines("Day21/input.txt");
+        //   lines = sample.Replace("\n", "").Split(['\r']);
+
+        rows = lines.Length;
+        cols = lines[0].Length;
+
+        var (start, grid) = ParseLines(lines);
+
+        System.Console.WriteLine(start);
+
+        List<Point2d> spots = [start];
+
+        for (int s = 0; s < 64; s++)
+        {
+            HashSet<Point2d> nextSpots = [];
+            foreach (var spot in spots)
+            {
+                for (int d = 0; d < 4; d++)
+                {
+                    var direction = (Direction)d;
+                    var next = Directions.From(direction, spot);
+                    if (!Directions.IsWithinBounds(next, rows, cols))
+                    {
+                        continue;
+                    }
+                    if (grid[next.x, next.y] == Rock)
+                    {
+                        continue;
+                    }
+                    if (!nextSpots.Add(next))
+                    {
+                        continue;
+                    }
+
+                    nextSpots.Add(next);
+                }
+            }
+
+            spots = [.. nextSpots];
+
+            //            Dump(grid, spots, System.Console.Out);
+        }
+
+
+
+
+        var total = spots.Count();
+
+        System.Console.WriteLine($"Answer ={total}");
+    }
+
+
+
+
+    private void Dump(byte[,] grid, IEnumerable<Point2d> path, TextWriter writer)
+    {
+        for (int r = 0; r < rows; r++)
+        {
+            writer.Write($"{r:000}: ");
+            for (int c = 0; c < cols; c++)
+            {
+                if (path.Contains((c, r)))
+                {
+                    System.Console.Write('O');
+                }
+                else
+                {
+                    char value = grid[c, r] switch
+                    {
+                        Rock => '#',
+                        Garden => '.'
+                    };
+                    writer.Write(value);
+                }
+            }
+            writer.WriteLine();
+        }
+        writer.WriteLine();
+    }
+
+    private (Point2d, byte[,]) ParseLines(string[] lines)
+    {
+        Point2d start = (0, 0);
+        var grid = new byte[cols, rows];
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                var a = lines[r][c];
+
+                if (a == 'S')
+                {
+                    start = (c, r);
+                    grid[c, r] = Garden;
+                }
+                else if (a == '#')
+                {
+                    grid[c, r] = Rock;
+                }
+                else if (a == '.')
+                {
+                    grid[c, r] = Garden;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        return (start, grid);
+    }
+
+    string sample = """
+                    ...........
+                    .....###.#.
+                    .###.##..#.
+                    ..#.#...#..
+                    ....#.#....
+                    .##..S####.
+                    .##..#...#.
+                    .......##..
+                    .##.#.####.
+                    .##..##.##.
+                    ...........
+                    """;
+}
